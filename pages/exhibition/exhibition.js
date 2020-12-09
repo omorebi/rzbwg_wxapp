d0de2fef2d3868b0b59d84dfb56ef33ee95eddcf
xiongluyang <xiongluyang>
2020-12-09 17:33:30 +0800

    首页

--- pages/exhibition/exhibition.js
+++ pages/exhibition/exhibition.js
@@ -1,15 +1,23 @@
 // pages/exhibition/exhibition.js
+const app = getApp();
+import {get_exhibition_category,get_exhibition_list} from '../../apis/api_index';
+
 Page({
 
     data: {
-
+      basicExhibitionList: [],
+      tempExhibitionList: [],
+      virtualExhibitonList: [],
+      currentIndex: 0
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+      this.getBasicExhibitionList();
+      this.getTempExhibitionList();
+      this.getVirtualExhibtionList();
     },
     /**
      * 跳转到具体展览页面
@@ -18,5 +26,37 @@ Page({
         wx.navigateTo({
           url: '/pages/exhibition/detailExhibition/detailExhibition',
         })
+    },
+    getBasicExhibitionList(){
+      let that = this;
+      get_exhibition_list(2).then(res => {
+        console.log(res.data.exhibitions.list)
+        that.setData({
+          basicExhibitionList: res.data.exhibitions.list
+        })
+      })
+    },
+    getTempExhibitionList(){
+      let that = this;
+      get_exhibition_list(3).then(res => {
+        console.log(res.data);
+        that.setData({
+          tempExhibitionList: res.data.exhibitions.list
+        })
+      })
+    },
+    getVirtualExhibtionList(){
+      let that = this;
+      get_exhibition_list(2).then(res => {
+        console.log(res.data.exhibitions.list);
+        that.setData({
+          virtualExhibitonList: res.data.exhibitions.list
+        })
+      })
+    },
+    swiperChange(e){
+      this.setData({
+        currentIndex: e.detail.current
+      })
     }
 })
